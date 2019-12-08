a6d37744507ca351f141f5178da3576d3e7cc95c
Nargiza O <nargiza2k@gmail.com>
2019-12-08 09:32:04 -0600

    displaying err.message on form with more functionality

---
+++ 5API/friday/lab/loginAuth/functions/index.js
@@ -0,0 +1,30 @@
+const functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.addAdminRole = functions.https.onCall((data, context) => {
+  // check if request is made by an admin
+  if (context.auth.token.admin !== true){
+    return { error: 'only admins can add other admins, haha:) '}
+  }
+  // get user and add custom claim (admin)
+  return admin.auth().getUserByEmail(data.email).then(user => {
+    return admin.auth().setCustomUserClaims(user.uid, {
+      admin: true
+    });
+  }).then(() => {
+    return {
+      message: `Success! ${data.email} has been made an admin`
+    }
+  }).catch(err => {
+    return err;
+  });
+});
--- 5API/friday/lab/loginAuth/scripts/auth.js
+++ 5API/friday/lab/loginAuth/scripts/auth.js
@@ -1,10 +1,24 @@
+// add admin cloud function
+const adminForm = document.querySelector('.admin-actions');
+adminForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const adminEmail = document.querySelector('#admin-email').value;
+  const addAdminRole = functions.httpsCallable('addAdminRole');
+  addAdminRole({ email: adminEmail }).then(result => {
+    console.log(result);
+  })
+})
+
 // listen for auth status changes
 auth.onAuthStateChanged( user => {
   if (user) {
+    user.getIdTokenResult().then(idTokenResult => {
+      user.admin = idTokenResult.claims.admin;
+      setupUI(user);
+    })
     // get data 
     db.collection('guides').onSnapshot((snapshot) => {
     setupGuides(snapshot.docs);
-    setupUI(user);
     }, err => console.log(err.message));
   } else {
     setupUI();
@@ -39,7 +53,7 @@ signupForm.addEventListener('submit', (e) => {
   const email = signupForm['signup-email'].value;
   const password = signupForm['signup-password'].value;
   
-  //sign up the user and fire it on database
+  // sign up the user and fire it on database
   auth.createUserWithEmailAndPassword(email, password).then(cred => {
     return db.collection('users').doc(cred.user.uid).set({
       bio: signupForm['signup-bio'].value
@@ -49,6 +63,9 @@ signupForm.addEventListener('submit', (e) => {
     const modal = document.querySelector('#modal-signup');
     M.Modal.getInstance(modal).close();
     signupForm.reset();
+    signupForm.querySelector('.error').innerHTML = '';
+  }).catch(err => {
+    signupForm.querySelector('.error').innerHTML = err.message
   })
 });
 
@@ -74,7 +91,9 @@ loginForm.addEventListener('submit', (e) => {
     const modal = document.querySelector('#modal-login');
     M.Modal.getInstance(modal).close();
     loginForm.reset()
-    
+    loginForm.querySelector('.error').innerHTML = '';
+  }).catch(err => {
+    loginForm.querySelector('.error').innerHTML = err.message;
   })
 })
 
