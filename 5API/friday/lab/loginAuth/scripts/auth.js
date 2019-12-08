7e6bf4efcf9cd451a7a20facd5291c2f78061a31
Nargiza O <nargiza2k@gmail.com>
2019-12-07 19:12:26 -0600

    create guide is added and rules are set up

--- 5API/friday/lab/loginAuth/scripts/auth.js
+++ 5API/friday/lab/loginAuth/scripts/auth.js
@@ -1,15 +1,33 @@
-// get data 
-db.collection('guides').get().then(snapshot => {
-  setupGuides(snapshot.docs);
-})
-
 // listen for auth status changes
 auth.onAuthStateChanged( user => {
   if (user) {
-    console.log('user logged in: ', user)
+    // get data 
+    db.collection('guides').get().then(snapshot => {
+    setupGuides(snapshot.docs);
+    setupUI(user);
+    })
   } else {
-    console.log("user logged out")
+    setupUI();
+    setupGuides([]);
   }
+});
+
+// create new guide
+const createForm = document.querySelector('#create-form')
+createForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  
+  db.collection('guides').add({
+    title: createForm['title'].value, // == createForm.title.value
+    content: createForm['content'].value
+  }).then(() => {
+    // close the modal and reset the form
+    const modal = document.querySelector('#modal-create')
+    M.Modal.getInstance(modal).close();
+    createForm.reset();
+  }).catch(err => {
+    console.log(err.message)
+  })
 })
 
 // signup the user
