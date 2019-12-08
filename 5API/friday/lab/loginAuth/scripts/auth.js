ec4958872c73ad2c8efde1dd28af1a0d6c1b56e7
Nargiza O <nargiza2k@gmail.com>
2019-12-07 20:49:18 -0600

    added bio on sign-up page and set the rules on firebase

--- 5API/friday/lab/loginAuth/scripts/auth.js
+++ 5API/friday/lab/loginAuth/scripts/auth.js
@@ -2,10 +2,10 @@
 auth.onAuthStateChanged( user => {
   if (user) {
     // get data 
-    db.collection('guides').get().then(snapshot => {
+    db.collection('guides').onSnapshot((snapshot) => {
     setupGuides(snapshot.docs);
     setupUI(user);
-    })
+    }, err => console.log(err.message));
   } else {
     setupUI();
     setupGuides([]);
@@ -35,14 +35,17 @@ const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (e) => {
   e.preventDefault();
   
-  // now get user info
+  // get user info
   const email = signupForm['signup-email'].value;
   const password = signupForm['signup-password'].value;
   
   //sign up the user and fire it on database
   auth.createUserWithEmailAndPassword(email, password).then(cred => {
-    
+    return db.collection('users').doc(cred.user.uid).set({
+      bio: signupForm['signup-bio'].value
+    });
     // close the modal 
+  }).then(() => {
     const modal = document.querySelector('#modal-signup');
     M.Modal.getInstance(modal).close();
     signupForm.reset();
@@ -65,9 +68,8 @@ loginForm.addEventListener('submit', (e) => {
   const email = loginForm['login-email'].value
   const password = loginForm['login-password'].value
   
-  // now let him login
+  // now let user login
   auth.signInWithEmailAndPassword(email, password).then(cred => {
-    
     // close the login modal and reset the form
     const modal = document.querySelector('#modal-login');
     M.Modal.getInstance(modal).close();
--- 5API/friday/lab/loginAuth/scripts/index.js
+++ 5API/friday/lab/loginAuth/scripts/index.js
@@ -1,13 +1,25 @@
 const guideList = document.querySelector('.guides');
 const loggedOutLinks = document.querySelectorAll('.logged-out');
 const loggedInLinks = document.querySelectorAll('.logged-in');
+const accountDetails = document.querySelector('.account-details');
 
 const setupUI = (user) => {
   if (user) {
-    // toggle UI elements
+    // account info
+    db.collection('users').doc(user.uid).get().then(doc => {
+      const html = `
+        <div>Logged in as ${user.email}</div>
+        <div>${doc.data().bio}</div>
+        `;
+      accountDetails.innerHTML = html;
+    })
+    
+    // toggle user UI elements
     loggedInLinks.forEach(item => item.style.display = 'block');
     loggedOutLinks.forEach(item => item.style.display = 'none');
   } else {
+    // hide account's info
+    accountDetails.innerHTML = '';
     // toggle UI elements
     loggedInLinks.forEach(item => item.style.display = 'none');
     loggedOutLinks.forEach(item => item.style.display = 'block');
